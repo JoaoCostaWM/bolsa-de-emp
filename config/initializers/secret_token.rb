3d12cc9c83de68c92a9634767db435e5a5e2718e
JoaoCostaWM <joao.costa@wiremaze.com>
2015-11-13 17:18:06 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BolsaDeEmpApp::Application.config.secret_key_base = 'c680788390d8b86e1e9b5f08744c0a168085da581af8930938f0a5391ab2a50ffe58bf49e16ce895eb4f5541daabaa7ff40e13a768c1a81c2d4f8bd622503d83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BolsaDeEmpApp::Application.config.session_store :cookie_store, key: '_bolsa_de_emp_app_session'
